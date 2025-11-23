1c6c0b87b45825d43bd313919599a79989a17fb3
hdhq1504 <hoquan15042004@gmail.com>
2025-11-24 00:48:01 +0700

    fix: Fix for prisma connection pool timeout on Vercel

--- server/src/prisma.js
+++ server/src/prisma.js
@@ -1,3 +1,20 @@
 import { PrismaClient } from "../src/generated/prisma/index.js";
-const prisma = new PrismaClient();
+
+const globalForPrisma = global;
+
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
 export default prisma;
